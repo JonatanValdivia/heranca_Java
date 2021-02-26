f7c09ab22f1d1b37ffbddc24955ee696be227256
Jonatan Vinicius Valdivia <jonatan.viniciusvaldivia@outlook.com>
2021-02-26 10:28:39 -0300

    Heranca em java (POO)

---
+++ heranca/src/heranca/model/Funcionario.java
@@ -0,0 +1,21 @@
+package heranca.model;
+
+import java.time.LocalDate;
+
+public class Funcionario extends Pessoa{
+	private double salario;
+	private LocalDate dataAdmissao;
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public LocalDate getDataAdmissao() {
+		return dataAdmissao;
+	}
+	public void setDataAdmissao(LocalDate dataAdmissao) {
+		this.dataAdmissao = dataAdmissao;
+	}
+	
+}
---
+++ heranca/src/heranca/teste/TestePessoa.java
@@ -0,0 +1,26 @@
+package heranca.teste;
+
+import java.time.LocalDate;
+
+import heranca.model.Funcionario;
+import heranca.model.Pessoa;
+
+public class TestePessoa {
+
+	public static void main(String[] args) {
+		Pessoa pessoa1 = new Pessoa();
+		pessoa1.setNome("Ana Cláudia");
+		pessoa1.setIdade(18);
+		pessoa1.setTelefone("4545-5454");		
+		System.out.println(pessoa1.getNome() + " fala-te: " + pessoa1.falarBomdia());
+		Funcionario funcionario1 = new Funcionario();
+		funcionario1.setNome("Gabriel");
+		funcionario1.setIdade(20);
+		funcionario1.setTelefone("90342-0932");
+		System.out.println(funcionario1.falarBomdia());
+		funcionario1.setSalario(1550.90);
+		funcionario1.setDataAdmissao(LocalDate.of(2000, 4, 24));
+		System.out.println(funcionario1.getIdade());
+	}
+
+}
